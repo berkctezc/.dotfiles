3229612bd7c7842d587435d95679f3b74aa90d5e
Berkcan Tezcaner <berkcan.tezcaner@gmail.com>
2024-02-23 18:13:51 +0300

    - some naught macOs things :) - nano noob btw :)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,23 @@
-if status is-interactive
+#if status is-interactive
     # Commands to run in interactive sessions can go here
-end
-
+#end
 if test (uname -s) = "Darwin"
+  # (╯°□°)╯︵ ┻━┻
+  alias apt="brew"
+  alias yum="brew"
+  alias dnf="brew"
+  alias pkg="brew"
+  alias i="brew install"
   set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
   set -gx PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
 end
+
+if [ -n $SSH_CONNECTION ]
+   export EDITOR='nano'
+else
+   export EDITOR='nano'
+end
+
+neofetch
 
 zoxide init fish | source
