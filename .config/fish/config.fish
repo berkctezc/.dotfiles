c4345b2278c40687ee19d4a445a3ae4501065d7f
Berkcan Tezcaner <berkcan.tezcaner@gmail.com>
2024-02-23 19:16:16 +0300

    atuin, other adjustments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,23 +1,23 @@
-#if status is-interactive
-    # Commands to run in interactive sessions can go here
-#end
-if test (uname -s) = "Darwin"
-  # (╯°□°)╯︵ ┻━┻
-  alias apt="brew"
-  alias yum="brew"
-  alias dnf="brew"
-  alias pkg="brew"
-  alias i="brew install"
-  set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-  set -gx PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
+if status is-interactive
+  atuin init fish | source
 end
-
-if [ -n $SSH_CONNECTION ]
-   export EDITOR='nano'
-else
-   export EDITOR='nano'
-end
-
-neofetch
 
 zoxide init fish | source
+
+if test (uname -s) = "Darwin"
+  alias copy pbcopy
+    # (╯°□°)╯︵ ┻━┻
+  alias apt brew
+  alias yum brew
+  alias dnf brew
+  alias pkg brew
+  alias i "brew install"
+  set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+  set -gx PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
+else
+  alias copy 'xclip -sel clip'5
+end
+
+export EDITOR='nano'
+
+neofetch
