267961ee869d324418bd4124b87136b7e8f47945
Berkcan Tezcaner <berkcan.tezcaner@gmail.com>
2024-02-23 12:53:12 +0300

    zoxide; macports installed

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+if test (uname -s) = "Darwin"
+  set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+  set -gx PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
+end
+
+zoxide init fish | source
