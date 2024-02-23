a87931c3e03d1285e352fbdab6cc3fd1a908dc89
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2024-02-23 19:52:17 +0300

    refactor script, fix mail

---
+++ scripts/brew_list.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DEVICE_NAME=$(hostname)
+GIT_MAIL=$(git config --global user.email)
+CLOUD_STORAGE='GoogleDrive'
+DESIRED_PATH='My Drive/Programs'
+CLOUD_STORAGE_FOLDER="$HOME/Library/CloudStorage/$CLOUD_STORAGE-$GIT_MAIL/$DESIRED_PATH"
+brew list >"$CLOUD_STORAGE_FOLDER/${DEVICE_NAME}_brew.txt"
