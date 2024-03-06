5e20b75c77438567fb3ca23821f6ededf2d4bf04
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2024-03-06 22:28:40 +0300

    restore it in one command

--- scripts/brew_restore.sh
+++ scripts/brew_restore.sh
@@ -2,7 +2,9 @@
 # please provide env var first
 pkg_list="$PACKAGES_FILE"
 
-while IFS= read -r package; do
-    echo "Installing package: $package"
-    brew install "$package"
-done < "$pkg_list"
+# while IFS= read -r package; do
+#     echo "Installing package: $package"
+#     brew install "$package"
+# done < "$pkg_list"
+
+brew install $(cat "$pkg_list")
