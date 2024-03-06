808312be92ac05183eceec37511f049051cb413b
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2024-03-06 22:24:56 +0300

    restore brew packages script

---
+++ scripts/brew_restore.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# please provide env var first
+pkg_list="$PACKAGES_FILE"
+
+while IFS= read -r package; do
+    echo "Installing package: $package"
+    brew install "$package"
+done < "$pkg_list"
