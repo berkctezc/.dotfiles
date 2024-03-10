ba9733c331fee999992c32498034b14c225d187d
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2024-03-10 16:52:40 +0300

    alias updt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,7 +15,7 @@ alias sudo 'sudo '
 alias week 'date +%V'
 alias chrome '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
 # Network
-alias ip "dig +short myip.opendns.com @resolver1.opendns.com"
+alias myip "dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip "ipconfig getifaddr en0"
 alias ips "ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
 alias scanlocal "arp -a"
