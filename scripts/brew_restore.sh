b81547abf80106289a23b2d3b1e65fc35eb7394f
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2024-03-16 20:57:17 +0300

    brew restore alternative loop

--- scripts/brew_restore.sh
+++ scripts/brew_restore.sh
@@ -8,3 +8,6 @@ pkg_list="$PACKAGES_FILE"
 # done < "$pkg_list"
 
 brew install $(cat "$pkg_list")
+
+# alternative with loop (for skipping packages that are not found)
+# while IFS= read -r package; do HOMEBREW_NO_AUTO_UPDATE=1 brew install "$package"; done < $pkg_list
