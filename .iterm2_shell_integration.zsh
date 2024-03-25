ee96c55a08c3aea398ab5bcfec2d50c240b1e10e
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2024-03-25 15:36:59 +0300

    cleanup

--- .iterm2_shell_integration.zsh
+++ .iterm2_shell_integration.zsh
@@ -1,17 +1,3 @@
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
 if [[ -o interactive ]]; then
   if [ "${ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX-}""$TERM" != "tmux-256color" -a "${ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX-}""$TERM" != "screen" -a "${ITERM_SHELL_INTEGRATION_INSTALLED-}" = "" -a "$TERM" != linux -a "$TERM" != dumb ]; then
     ITERM_SHELL_INTEGRATION_INSTALLED=Yes
@@ -29,11 +15,6 @@ if [[ -o interactive ]]; then
       printf "\033]1337;SetUserVar=%s=%s\007" "$1" $(printf "%s" "$2" | base64 | tr -d '\n')
     }
 
-    # Users can write their own version of this method. It should call
-    # iterm2_set_user_var but not produce any other output.
-    # e.g., iterm2_set_user_var currentDirectory $PWD
-    # Accessible in iTerm2 (in a badge now, elsewhere in the future) as
-    # \(user.currentDirectory).
     whence -v iterm2_print_user_vars > /dev/null 2>&1
     if [ $? -ne 0 ]; then
       iterm2_print_user_vars() {
