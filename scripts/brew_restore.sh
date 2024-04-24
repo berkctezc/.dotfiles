1d2cb0d8690933c97aa2e56d2e8a9c6c3dcd2f1b
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2024-04-24 10:28:45 +0300

    brew restore script loop turned on

--- scripts/brew_restore.sh
+++ scripts/brew_restore.sh
@@ -2,12 +2,12 @@
 # please provide env var first
 pkg_list="$PACKAGES_FILE"
 
-# while IFS= read -r package; do
-#     echo "Installing package: $package"
-#     brew install "$package"
-# done < "$pkg_list"
+while IFS= read -r package; do
+  echo "Installing package: $package"
+  brew install "$package"
+done < "$pkg_list"
 
-brew install $(cat "$pkg_list")
+#brew install $(cat "$pkg_list")
 
 # alternative with loop (for skipping packages that are not found)
-# while IFS= read -r package; do HOMEBREW_NO_AUTO_UPDATE=1 brew install "$package"; done < $pkg_list
+while IFS= read -r package; do HOMEBREW_NO_AUTO_UPDATE=1 brew install "$package"; done < $pkg_list
