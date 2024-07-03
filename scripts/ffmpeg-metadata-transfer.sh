976d7b34333e8300b6fe6e168ae1f237d5c3ed69
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2024-07-03 23:08:19 +0300

    ca certs fix & ffmpeg metadata transfer scripts are added to scripts folder

---
+++ scripts/ffmpeg-metadata-transfer.sh
@@ -0,0 +1,1 @@
+ffmpeg -i in.mp4 -i out.mp4 -map 1 -map_metadata 0 -c copy fixed.mp4
---
+++ scripts/fix-ca-certs.sh
@@ -0,0 +1,9 @@
+sudo touch /etc/ssl/certs/ca-certificates.crt;
+
+sudo wget -O /etc/ssl/certs/ca-certificates.crt https://curl.se/ca/cacert.pem;
+
+sudo ln -sf /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-bundle.crt;
+
+cat /etc/ssl/certs/ca-certificates.crt;
+
+sudo chmod 644 /etc/ssl/certs/ca-certificates.crt;
