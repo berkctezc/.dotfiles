2f573ab7354b121b8c04cbf442a2c07dcd60a736
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2024-07-04 14:39:43 +0300

    oh my scripts

---
+++ scripts/oh-my-x.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# oh-my-bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)";
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+# oh-my-fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish;
