7bed70ceac92d60e2950eb560c63b74eb913df75
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2024-09-24 19:26:23 +0300

    parallelize for loop

--- scripts/brew_restore.sh
+++ scripts/brew_restore.sh
@@ -3,11 +3,12 @@
 pkg_list="${PACKAGES_FILE}"
 
 while IFS= read -r package; do
-  echo "Installing package: $package"
-  brew install "$package"
+  (
+  	echo "Installing package: $package";
+	HOMEBREW_NO_AUTO_UPDATE=1
+  	brew install "$package";
+  	) &
 done < "$pkg_list"
+wait
 
 #brew install $(cat "$pkg_list")
-
-# alternative with loop (for skipping packages that are not found)
-while IFS= read -r package; do HOMEBREW_NO_AUTO_UPDATE=1 brew install "$package"; done < $pkg_list
